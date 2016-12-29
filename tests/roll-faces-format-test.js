89fe594b57956d0c64c65dea881e157278163593
Robert Dennis <rdennis.dev@gmail.com>
2016-12-28 22:24:47 -0600

    Allow faces to contain anything except |, [, or ]

--- tests/roll-faces-format-test.js
+++ tests/roll-faces-format-test.js
@@ -12,18 +12,27 @@ describe('DiceRoller - [ faces ] format', () => {
             .that.is.oneOf(['cat', 'dog', 'fish']);
     });
 
-    it('roll("[cat|dog|fish]") should return an object with a faces property of [cat, dog, fish]', () => {
-        let diceRoller = new DiceRoller();
-        expect(diceRoller.roll('[cat|dog|fish]')).to.have.property('faces')
-            .that.is.eql(['cat', 'dog', 'fish']);
-    });
+    // faces tests
+    let tests = [
+        ['cat', 'dog', 'fish'],
+        [' cat ', ' dog ', ' fish '],
+        ['thing-1', 'thing-2', 'thing-3'],
+        ['$', '******', '¯\\_(ツ)_/¯']
+    ];
 
-    it('roll("[ thing one | thing two ]") should return an object with a result property of thing one, or thing two', () => {
-        let diceRoller = new DiceRoller();
-        expect(diceRoller.roll('[ thing one | thing two ]')).to.have.property('result')
-            .that.is.oneOf(['thing one', 'thing two']);
-    });
+    for (let i = 0, l = tests.length; i < l; i++) {
+        let test = tests[i];
+        let input = `[${test.join('|')}]`;
+        let expected = test.map(x => x.trim());
 
+        it(`roll("${input}") should return an object with a faces property of [${expected.join(', ')}]`, () => {
+            let diceRoller = new DiceRoller();
+            expect(diceRoller.roll(input)).to.have.property('faces')
+                .that.is.eql(expected);
+        });
+    }
+
+    // repetition test
     it('roll("[cat|dog|fish]") should not return the same result 50 times in a row', () => {
         let diceRoller = new DiceRoller();
         let results = [];
