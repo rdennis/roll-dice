1ce41d511eb031ad5ae84ecd1688f7541151e55b
Robert Dennis <rdennis@doctorlogic.com>
2016-12-29 21:11:50 -0600

    Update example.js

--- example.js
+++ example.js
@@ -1,4 +1,10 @@
 let DiceRoller = require('roll-dice');
 let diceRoller = new DiceRoller();
 
-let result = diceRoller('d20');
+let result1 = diceRoller.roll('d20');
+let result2 = diceRoller.roll('2d8+2');
+let result3 = diceRoller.roll('[ cat | dog | fish ]');
+
+console.dir(result1);
+console.dir(result2);
+console.dir(result3);
