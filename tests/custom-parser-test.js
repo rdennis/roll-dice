15d780e6e30dfa124899766bf8d1e562c524cdef
Robert Dennis <rdennis.dev@gmail.com>
2017-01-05 01:52:28 -0600

    Added tests for custom parser functionality

---
+++ tests/custom-parser-test.js
@@ -0,0 +1,50 @@
+// tests/roll-d-format-test.js
+'use strict';
+
+let chai = require('chai');
+let expect = chai.expect;
+let DiceRoller = require('./../src/dice-roller');
+let RollUtil = require('./../src/roll-util');
+
+describe('Custom Parser', () => {
+    let customMatcher = /^(\d+)$/;
+    let customPerser = (input) => {
+        let match = customMatcher.exec(input);
+
+        if(match === null) {
+            return new DiceRoller.InvalidInputError(input);
+        }
+
+        let faces = parseInt(match[1], 10);
+        
+        if(faces < 2) {
+            return new DiceRoller.InvalidInputError(input);
+        }
+
+        let result = RollUtil.getRandomInt(1, faces);
+
+        return {
+            result,
+            faces
+        };
+    };
+
+    it('diceRoller.parsers should accept a new parser', () => {
+        let diceRoller = new DiceRoller();
+        diceRoller.parsers.push(customPerser);
+
+        expect(diceRoller).to.have.property('parsers')
+            .and.to.contain(customPerser);
+    });
+
+    it('roll("20") should use the custom parser', () => {
+        let diceRoller = new DiceRoller();
+        diceRoller.parsers.push(customPerser);
+
+        let result = diceRoller.roll('20');
+
+        expect(result).to.not.be.an.instanceOf(DiceRoller.InvalidInputError);
+        expect(result).to.have.property('result').within(1, 20);
+        expect(result).to.have.property('faces', 20);
+    });
+});
